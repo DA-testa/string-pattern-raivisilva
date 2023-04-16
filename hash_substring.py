ccc1cc58ef40318d0d23e958bed216253c259150
Raivis Ilva <raivis.ilva@edu.rtu.lv>
2023-04-16 22:27:17 +0300

    Tagad pabeigts?

--- hash_substring.py
+++ hash_substring.py
@@ -4,8 +4,7 @@ def read_input():
     izvele = input()
 
     if "I" in izvele:
-        pattern = input().rstrip()
-        text = input().rstrip()
+        pattern, text = input().rstrip(), input().rstrip()
         if 1 <= len(pattern) <= len(text) <= (5 * 10**5):
             return (pattern, text)
 
@@ -15,19 +14,15 @@ def read_input():
             text = f.readline().rstrip()
         if 1 <= len(pattern) <= len(text) <= (5 * 10**5):
             return (pattern, text)
-    
+
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
+
 def get_occurrences(pattern, text):
-    pattern_length = len(pattern)
-    text_length = len(text)
-    prime_number = 101
-    pattern_hash = 0  
-    text_hash = 0   
-    repetitions = []
+    pattern_length, text_length = len(pattern), len(text)
+    prime_number, pattern_hash, text_hash, repetitions = 101, 0, 0, []
 
     for i in range(pattern_length):
         pattern_hash += ord(pattern[i]) * pow(prime_number, i)
@@ -38,12 +33,13 @@ def get_occurrences(pattern, text):
             repetitions.append(i)
 
         if i < text_length - pattern_length:
-            text_hash = (text_hash - ord(text[i]) * pow(prime_number, 0)) // prime_number
-            text_hash += ord(text[i+pattern_length]) * pow(prime_number, pattern_length-1)
+            text_hash = (
+                text_hash - ord(text[i]) * pow(prime_number, 0)) // prime_number
+            text_hash += ord(text[i+pattern_length]) * \
+                pow(prime_number, pattern_length-1)
 
     return repetitions
 
-# this part launches the functions
+
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
