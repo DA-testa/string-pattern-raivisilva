2108a10c2ed0dd3d5bc60afd58d79dd832e4e99b
Raivis Ilva <raivis.ilva@edu.rtu.lv>
2023-04-16 21:53:31 +0300

    atpakaļ

--- hash_substring.py
+++ hash_substring.py
@@ -24,23 +24,24 @@ def print_occurrences(output):
 def get_occurrences(pattern, text):
     pattern_length = len(pattern)
     text_length = len(text)
-    prime_numbers = 31  
+    prime_number = 101
     pattern_hash = 0  
     text_hash = 0   
-    repeat = []
+    repetitions = []
 
     for i in range(pattern_length):
-        pattern_hash = (pattern_hash << 1) + ord(pattern[i])
-        text_hash = (text_hash << 1) + ord(text[i])
+        pattern_hash += ord(pattern[i]) * pow(prime_number, i)
+        text_hash += ord(text[i]) * pow(prime_number, i)
 
     for i in range(text_length - pattern_length + 1):
         if pattern_hash == text_hash and pattern == text[i:i+pattern_length]:
-            repeat.append(i)
+            repetitions.append(i)
 
         if i < text_length - pattern_length:
-            text_hash = ((text_hash - ord(text[i]) * (prime_numbers ** (pattern_length - 1))) << 1) + ord(text[i+pattern_length])
+            text_hash = (text_hash - ord(text[i]) * pow(prime_number, 0)) // prime_number
+            text_hash += ord(text[i+pattern_length]) * pow(prime_number, pattern_length-1)
 
-    return repeat
+    return repetitions
 
 # this part launches the functions
 if __name__ == '__main__':
