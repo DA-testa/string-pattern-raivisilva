cdb4e9632fbde1544e0a8535c71ca9c4cbd30ea0
Raivis Ilva <raivis.ilva@edu.rtu.lv>
2023-04-16 17:42:16 +0300

    Ievades mock up

--- hash_substring.py
+++ hash_substring.py
@@ -3,18 +3,24 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    izvele = input()
 
+    if "I" in izvele:
+        pattern = input().rstrip()
+        text = input().rstrip()
+        return (pattern, text)
+
+    elif "F" in izvele:
+        faila_nosaukums = input()
+        if 'a' in faila_nosaukums:
+            return
+        else:
+            with open("./tests/06", mode='r') as f:
+                pattern = f.readline().rstrip()
+                text = f.readline().rstrip()
+                return (pattern, text)
+    
+    
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
